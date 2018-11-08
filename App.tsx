d15f21815a5b5bb67dbc0f71f1b8e5c348b5e5a7
zhoudonghua <donghuazh66@gmail.com>
2018-11-08 15:55:45 +0800

    Initial repository && commit

---
+++ App.tsx
@@ -0,0 +1,59 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow
+ */
+
+import * as React from "react";
+import { Platform, StyleSheet, Text, TextStyle, View, ViewStyle } from "react-native";
+
+import { observer } from "mobx-react/native";
+
+const instructions = Platform.select({
+  ios: "Press Cmd+R to reload,\n" + "Cmd+D or shake for dev menu",
+  android: "Double tap R on your keyboard to reload,\n" + "Shake or press menu button for dev menu"
+});
+
+interface InterfaceProps {}
+
+interface InterfaceState {}
+
+interface InterfaceStyle {
+  container: ViewStyle;
+  welcome: TextStyle;
+  instructions: TextStyle;
+}
+
+@observer
+export default class App extends React.Component<InterfaceProps, InterfaceState> {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.welcome}>Welcome to React Native!</Text>
+        <Text style={styles.instructions}>To get started, edit App.tsx</Text>
+        <Text style={styles.instructions}>{instructions}</Text>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create<InterfaceStyle>({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#F5FCFF"
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: "center",
+    margin: 10
+  },
+  instructions: {
+    textAlign: "center",
+    color: "#333333",
+    marginBottom: 5
+  }
+});
---
+++ README.md
@@ -0,0 +1,34 @@
+## react-native-ts-mobx
+
+Blank app base on react-native, typescript, mobx.
+
+### Build with
+
+- [x] react 16.6.0
+- [x] react-native 0.57.4
+- [x] typescript 3.1.6
+- [x] mobx 5.6.0
+- [ ] react-navigation 2.18.2
+- [ ] ts-jest 23.6.0
+
+### Try it yourself
+
+1. Clone and Install
+
+```bash
+# Clone the repo
+git clone https://github.com/donghuazh/react-native-ts-mobx.git && cd $_
+
+# Install dependencies
+yarn install
+```
+
+2. Run it on iOS or Android
+
+```bash
+# Run on iOS
+yarn ios
+
+# Run on Android
+yarn android
+```
