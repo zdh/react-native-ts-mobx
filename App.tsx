10ee1ba0900f539fe23ba6ee7a2d47a7ac75dbe7
zhoudonghua <donghuazh66@gmail.com>
2018-11-10 00:55:23 +0800

    set the background color to #4F6D7A, the text color to #F5FCFF, and use light text in the status bar.

--- App.tsx
+++ App.tsx
@@ -7,13 +7,13 @@
  */
 
 import * as React from "react";
-import { Platform, StyleSheet, Text, TextStyle, View, ViewStyle } from "react-native";
+import { Platform, StyleSheet, Text, View, StatusBar, ViewStyle, TextStyle } from "react-native";
 
 import { observer } from "mobx-react/native";
 
 const instructions = Platform.select({
   ios: "Press Cmd+R to reload,\n" + "Cmd+D or shake for dev menu",
-  android: "Double tap R on your keyboard to reload,\n" + "Shake or press menu button for dev menu"
+  android: "Double tap R on your keyboard to reload,\n" + "Shake or press menu button for dev menu",
 });
 
 interface InterfaceProps {}
@@ -31,6 +31,7 @@ export default class App extends React.Component<InterfaceProps, InterfaceState>
   render() {
     return (
       <View style={styles.container}>
+        <StatusBar barStyle="light-content" backgroundColor="#4F6D7A" />
         <Text style={styles.welcome}>Welcome to React Native!</Text>
         <Text style={styles.instructions}>To get started, edit App.tsx</Text>
         <Text style={styles.instructions}>{instructions}</Text>
@@ -44,16 +45,17 @@ const styles = StyleSheet.create<InterfaceStyle>({
     flex: 1,
     justifyContent: "center",
     alignItems: "center",
-    backgroundColor: "#F5FCFF"
+    backgroundColor: "#4F6D7A",
   },
   welcome: {
     fontSize: 20,
     textAlign: "center",
-    margin: 10
+    margin: 10,
+    color: "#F5FCFF",
   },
   instructions: {
     textAlign: "center",
-    color: "#333333",
-    marginBottom: 5
-  }
+    color: "#F5FCFF",
+    marginBottom: 5,
+  },
 });
